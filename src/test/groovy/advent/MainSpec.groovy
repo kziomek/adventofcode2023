28462a329d5433e3987364e9f48b918aa57fc335
Krzysztof Ziomek <kziomek@onet.eu>
2024-05-25 17:06:14 +0100

    Init commit

---
+++ src/test/groovy/advent/MainSpec.groovy
@@ -0,0 +1,13 @@
+package advent
+
+import spock.lang.Specification
+
+
+class MainSpec extends Specification {
+
+    def "one plus one should equal two"() {
+        expect:
+        1 + 1 == 2
+    }
+
+}
