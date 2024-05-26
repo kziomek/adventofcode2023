1f4e7d0c104f8e59c0932bacf060939eece01a17
Krzysztof Ziomek <kziomek@onet.eu>
2024-05-26 15:50:38 +0100

    Day 2 - part 2

--- src/main/java/advent/day2/CubeConundrum.java
+++ src/main/java/advent/day2/CubeConundrum.java
@@ -2,8 +2,11 @@ package advent.day2;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class CubeConundrum {
@@ -11,12 +14,48 @@ public class CubeConundrum {
     public static void main(String[] args) throws IOException {
 
 //        List<Game> games = Parser.parse(Paths.get("src/main/resources/day2/example-part1.txt"));
-        List<Game> games = Parser.parse(Paths.get("src/main/resources/day2/my-input.txt"));
+                List<Game> games = Parser.parse(Paths.get("src/main/resources/day2/my-input.txt"));
 
-        System.out.println(runPart1(games));
+        //        System.out.println(runPart1(games));
+        System.out.println(runPart2(games));
     }
 
-    private static Integer runPart1(List<Game> games){
+    private static Integer runPart2(List<Game> games) {
+        Set<Map<String, Integer>> gameMinCubeCounts = collectMinimumRequiredCubeNumbers(games);
+        return calculateSumOfPowers(gameMinCubeCounts);
+    }
+
+    private static int calculateSumOfPowers(Set<Map<String, Integer>> gameMinCubeCounts) {
+        return gameMinCubeCounts.stream()
+            .map(gameMinCubeCount -> gameMinCubeCount
+                .values()
+                .stream()
+                .reduce(1, (a, b) -> a * b))
+            .mapToInt(i -> i)
+            .sum();
+    }
+
+    private static Set<Map<String, Integer>> collectMinimumRequiredCubeNumbers(List<Game> games) {
+        Set<Map<String, Integer>> gameMinCubeCounts = new HashSet<>();
+        for (Game game : games) {
+            Map<String, Integer> gameMap = new HashMap<>();
+            for (Map<String, Integer> setOfHand : game.setsOfHands) {
+                if (setOfHand.containsKey("red")) {
+                    gameMap.merge("red", setOfHand.get("red"), (v1, v2) -> v2 > v1 ? v2 : v1);
+                }
+                if (setOfHand.containsKey("green")) {
+                    gameMap.merge("green", setOfHand.get("green"), (v1, v2) -> v2 > v1 ? v2 : v1);
+                }
+                if (setOfHand.containsKey("blue")) {
+                    gameMap.merge("blue", setOfHand.get("blue"), (v1, v2) -> v2 > v1 ? v2 : v1);
+                }
+            }
+            gameMinCubeCounts.add(gameMap);
+        }
+        return gameMinCubeCounts;
+    }
+
+    private static Integer runPart1(List<Game> games) {
         return games.stream()
             .filter(filterGamePredicate())
             .mapToInt(game -> game.id)
@@ -39,5 +78,4 @@ public class CubeConundrum {
             return true;
         };
     }
-
 }
