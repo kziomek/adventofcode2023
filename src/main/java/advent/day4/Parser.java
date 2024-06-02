9ad0f2fbc115fa1b46f3346302abb2ae3c624b8c
Krzysztof Ziomek <kziomek@onet.eu>
2024-06-02 14:50:34 +0100

    Day 4 - Scratchcard - part 1

---
+++ src/main/java/advent/day4/Parser.java
@@ -0,0 +1,28 @@
+package advent.day4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Parser {
+
+    public static List<Card> parse(String path) throws IOException {
+        return Files.readAllLines(Path.of(path))
+            .stream()
+            .map(line -> {
+                String[] numberLists = line.split(":")[1].split("\\|");
+                return new Card(parseNumbers(numberLists[0]), parseNumbers(numberLists[1]));
+            }).toList();
+    }
+
+    private static Set<Integer> parseNumbers(String numberList) {
+        return Arrays.stream(numberList.trim().split(" "))
+            .filter( s -> !s.isEmpty())
+            .map(Integer::valueOf)
+            .collect(Collectors.toSet());
+    }
+}
