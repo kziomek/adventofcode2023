b531e98d9bd732b32a4205ef19831f50f8d433c0
Krzysztof Ziomek <kziomek@onet.eu>
2024-06-22 18:02:41 +0100

    Day 6 - Wait For It - part 1 - calculate with quadratic equation

--- src/main/java/advent/day6/WaitForIt.java
+++ src/main/java/advent/day6/WaitForIt.java
@@ -1,19 +1,14 @@
 package advent.day6;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.IntBinaryOperator;
 
 public class WaitForIt {
 
     public static void main(String[] args) throws IOException {
 
-//        List<RecordRace> recordRaces = Parser.parse("src/main/resources/day6/example.txt");
-        List<RecordRace> recordRaces = Parser.parse("src/main/resources/day6/my-input.txt");
+        //        List<RecordRace> recordRaces = Parser.parse("src/main/resources/day6/example.txt");
+        List<RecordRace> recordRaces = Parser.parse("src/main/resources/day6/my-input.txt"); // part-1 512295
 
         int result = recordRaces
             .stream()
@@ -23,6 +18,33 @@ public class WaitForIt {
             .reduce(1, (a, b) -> a * b);
 
         System.out.println(result);
+    }
+
+    /*
+         https://www.youtube.com/watch?v=YYGlwDC-5gA&ab_channel=MarekDuda
+         x * (time - x) - maxDistance = 0
+         -x2 +x*time - maxDistance = 0
+         delta = time*time - 4 * (-1 ) * maxDistance
+         sqrt delta
+         x1 =  (-time + sqrtDelta) / -2
+         x2 =  (-time - sqrtDelta) / -2
+     */
+
+    private static int calculateWithQuadraticEquation(RecordRace recordRace) {
+        int time = recordRace.getTime();
+        int maxDistance = recordRace.getDistance();
+        int delta = time * time - 4 * maxDistance;
+        double sqrtDelta = Math.sqrt(delta);
+
+        double x1 = (-time + sqrtDelta) / -2;
+        double x2 = (-time - sqrtDelta) / -2;
+
+        x1 = x1 == Math.floor(x1) ? x1 + 1 : Math.ceil(x1);
+        x2 = x2 == Math.ceil(x2) ? x2 - 1 : Math.floor(x2);
+
+        double r = x2 - x1 + 1;
+
+        return (int) r;
     }
 
     private static int calculate(RecordRace recordRace) {
