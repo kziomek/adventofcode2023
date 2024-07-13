9113b4c064f8d46232445689805e09fd944a3e68
Krzysztof Ziomek <kziomek@onet.eu>
2024-07-13 15:53:45 +0100

    Day 11 - Cosmic Expansion - part 1

--- src/main/java/advent/day11/CosmicExpansion.java
+++ src/main/java/advent/day11/CosmicExpansion.java
@@ -1,19 +1,38 @@
 package advent.day11;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CosmicExpansion {
 
     public static void main(String[] args) throws IOException {
-        char[][] arr = Parser.parse("src/main/resources/day11/example.txt");
+//        char[][] arr = Parser.parse("src/main/resources/day11/example.txt");
+        char[][] arr = Parser.parse("src/main/resources/day11/my-input.txt");
         char[][] expanded = Expander.expand(arr);
 
         print(expanded);
+
+        List<Galaxy> galaxies = new ArrayList<>();
+        for (int i = 0; i < expanded.length; i++) {
+            for (int j = 0; j < expanded[0].length; j++) {
+                if (expanded[i][j] == '#') {
+                    galaxies.add(new Galaxy(i, j));
+                }
+            }
+        }
+        Galaxy[] gals = galaxies.toArray(Galaxy[]::new);
+
+        long sum = 0;
+        for (int i = 0; i < gals.length; i++) {
+            for (int j = i + 1; j < gals.length; j++) {
+                sum += Math.abs(gals[i].i - gals[j].i) + Math.abs(gals[i].j - gals[j].j);
+            }
+        }
+        System.out.println("Sum " + sum);
     }
-
-
 
     private static void print(char[][] arr) {
         for (char[] chars : arr) {
---
+++ src/main/java/advent/day11/Galaxy.java
@@ -0,0 +1,10 @@
+package advent.day11;
+
+public class Galaxy {
+    int i,j;
+
+    public Galaxy(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
