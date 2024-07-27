551d6369f4ae0a6f69549bb6e930cbae7f54a704
Krzysztof Ziomek <kziomek@onet.eu>
2024-07-27 10:13:39 +0100

    Day 12 - HotSprings - part 2 - working algorithm

--- src/main/java/advent/day12/HotSprings.java
+++ src/main/java/advent/day12/HotSprings.java
@@ -3,22 +3,36 @@ package advent.day12;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.SQLOutput;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class HotSprings {
 
+    static int operationCount = 0;
+
     public static void main(String[] args) throws IOException {
-        int resultCount = 0;
+        long resultCount = 0;
         int inputCount = 0;
-        List<String> inputs = Files.readAllLines(Path.of("src/main/resources/day12/my-input.txt"));
+        //        List<String> inputs = Collections.singletonList(".??..??...?##. 1,1,3"); // 63  operations
+        //        List<String> inputs = Collections.singletonList("?###???????? 3,2,1");
+        //                List<String> inputs = Collections.singletonList("??????????#??.?????? 3,4,2,1,1,1"); // 524287  operations
+        //                        List<String> inputs = Collections.singletonList("????.#...#...?????.#...#...?????.#...#...?????.#...#...?????.#...#... 4,1,1,4,1,1,4,1,1,4,1,1,4,1,1"); // 524287  operations
+//        List<String> inputs = Collections.singletonList("?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"); // 524287  operations
+//                                List<String> inputs = Files.readAllLines(Path.of("src/main/resources/day12/example.txt"));
+
+                                List<String> inputs = Files.readAllLines(Path.of("src/main/resources/day12/my-input.txt"));
         for (String input : inputs) {
-            inputCount++;
-            System.out.println(input);
-            System.out.println(inputCount);
-            resultCount += matches(input);
+            System.out.println("Process: " + input);
+            RowRecord record = new RowRecord(input);
+            record.unfold();
+            resultCount += matches(record, new HashMap<>());
         }
 
         System.out.println(resultCount);
+        System.out.println(operationCount);
     }
 
     public static void count(String s) {
@@ -33,12 +47,42 @@ public class HotSprings {
         }
     }
 
-    public static int matches(String input) {
-//        System.out.println(input);
-        if (!input.contains("?")) {
-            return new RowRecord(input).isValid() ? 1 : 0;
+    public static long matches(RowRecord record, HashMap<String, Long> mem) {
+
+        record.print();
+        record.removeLeadingDots();
+        operationCount++;
+        int groupSize = record.firstGroupSize();
+
+        if (groupSize == 0) {
+            return record.validationList.isEmpty() ? 1 : 0;
         }
 
-        return matches(input.replaceFirst("\\?", ".")) + matches(input.replaceFirst("\\?", "#"));
+        if (groupSize > 0) {
+            Integer expectedGroupSize = record.validationList.poll();
+            record.removeLeadingGroup();
+            return expectedGroupSize != null && expectedGroupSize == groupSize ? matches(record, mem) : 0;
+        }
+
+        if (groupSize == -1) {
+            String conditionRecord = record.conditionRecord;
+
+            if (mem.containsKey(record.key())) {
+                System.out.println("Hit cache");
+                return mem.get(record.key());
+            }
+            long result = matches(new RowRecord(conditionRecord.replaceFirst("\\?", "."), new LinkedList<>(record.validationList)), mem)
+                + matches(new RowRecord(conditionRecord.replaceFirst("\\?", "#"), new LinkedList<>(record.validationList)), mem);
+            mem.put(record.key(), result);
+            return result;
+        }
+
+        //        System.out.println(input);
+        //        if (!input.contains("?")) {
+        //            return new RowRecord(input).isValid() ? 1 : 0;
+        //        }
+        //
+        //        return matches(input.replaceFirst("\\?", ".")) + matches(input.replaceFirst("\\?", "#"));
+        throw new IllegalStateException("unexpected");
     }
 }
--- src/main/java/advent/day12/RowRecord.java
+++ src/main/java/advent/day12/RowRecord.java
@@ -2,47 +2,125 @@ package advent.day12;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class RowRecord {
 
     String conditionRecord;
-    List<Integer> validationList;
+    Queue<Integer> validationList;
 
     public RowRecord(String input) {
         String[] parts = input.split(" ");
-        validationList = Arrays.stream(parts[1].split(",")).map(Integer::valueOf).toList();
+        validationList = new LinkedList<>(Arrays.stream(parts[1].split(",")).map(Integer::valueOf).toList());
         conditionRecord = parts[0];
     }
 
-    public boolean isValid() {
-        List<Integer> groups = new ArrayList<>();
-
-        int count = 0;
-        for (int i = 0; i < conditionRecord.length(); i++) {
-            char c = conditionRecord.charAt(i);
-
-            if (c == '#') {
-                count++;
-            }
-            if (c == '.' && count > 0) {
-                groups.add(count);
-                count = 0;
-            }
-        }
-        if (count > 0) {
-            groups.add(count);
-        }
-
-        if (validationList.size() != groups.size()) {
-            return false;
-        }
-        for (int i = 0; i < validationList.size(); i++) {
-            if (validationList.get(i) != groups.get(i)) {
-                return false;
-            }
-        }
-
-        return true;
+    public RowRecord(String conditionRecord, Queue<Integer> validationList) {
+        this.conditionRecord = conditionRecord;
+        this.validationList = validationList;
     }
+
+    public void removeLeadingGroup() {
+        int i = 0;
+        while (i < conditionRecord.length() && conditionRecord.charAt(i) == '#') {
+            i++;
+        }
+        conditionRecord = conditionRecord.substring(i);
+    }
+
+    public void removeLeadingDots() {
+        int i = 0;
+        while (i < conditionRecord.length() && conditionRecord.charAt(i) == '.') {
+            i++;
+        }
+        conditionRecord = conditionRecord.substring(i);
+        //        if (i == conditionRecord.length() ){
+        //            conditionRecord = "";
+        //        } else {
+        //            conditionRecord = conditionRecord.substring(i);
+        //        }
+
+    }
+
+    // return -1 when ? present in group
+    public int firstGroupSize() {
+
+        int i = 0;
+
+        // skip .
+        while (i < conditionRecord.length() && conditionRecord.charAt(i) == '.') {
+            i++;
+        }
+
+        if (i == conditionRecord.length()) {
+            return 0;
+        }
+
+        int groupSize = 0;
+        for (int j = i; j < conditionRecord.length(); j++) {
+            if (conditionRecord.charAt(j) == '#') {
+                groupSize++;
+            }
+            if (conditionRecord.charAt(j) == '.') {
+                break;
+            }
+            if (conditionRecord.charAt(j) == '?') {
+                return -1; // group not ready
+            }
+        }
+        return groupSize;
+    }
+
+    public void print() {
+        System.out.println(key());
+    }
+
+    public String key() {
+        return conditionRecord + " " + validationList;
+    }
+
+    public void unfold() {
+        conditionRecord = conditionRecord + "?" + conditionRecord + "?" + conditionRecord + "?" + conditionRecord + "?" + conditionRecord;
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addAll(validationList);
+        list.addAll(validationList);
+        list.addAll(validationList);
+        list.addAll(validationList);
+        list.addAll(validationList);
+
+        validationList = list;
+    }
+
+    //    public boolean isValid() {
+    //        List<Integer> groups = new ArrayList<>();
+    //
+    //        int count = 0;
+    //        for (int i = 0; i < conditionRecord.length(); i++) {
+    //            char c = conditionRecord.charAt(i);
+    //
+    //            if (c == '#') {
+    //                count++;
+    //            }
+    //            if (c == '.' && count > 0) {
+    //                groups.add(count);
+    //                count = 0;
+    //            }
+    //        }
+    //        if (count > 0) {
+    //            groups.add(count);
+    //        }
+    //
+    //        if (validationList.size() != groups.size()) {
+    //            return false;
+    //        }
+    //        for (int i = 0; i < validationList.size(); i++) {
+    //            if (validationList.get(i) != groups.get(i)) {
+    //                return false;
+    //            }
+    //        }
+    //
+    //        return true;
+    //    }
 }
